ffc3077abfd5f8e707a29a579c24df1ca740e2bb
dg <dg@ops>
2021-05-01 13:39:32 -0700

    adding dip rsvps

--- firmware/main/dip.h
+++ firmware/main/dip.h
@@ -12,11 +12,23 @@
 #define DIP9 (1 << 15)
 #define DIP10 (1 << 14)
 
+// DIP SETTINGS 0-1023
+
 #define DIP_CODE(code) (((code & 0b11) << 14) | (code >> 2))
 
 #define CODE_UNLOCK                      (DIP_CODE(951))
 #define CODE_BUZZER_MORSE                (DIP_CODE(420))
 #define CODE_BUZZER_ADVANCED_MORSE       (DIP_CODE(360))
+
+#define CODE_LED_IMG_BINARY              (DIP_CODE(164))
+#define CODE_LED_IMG_REDVAR              (DIP_CODE(864))
+#define CODE_LED_IMG_RGB                 (DIP_CODE(537))
+#define CODE_LED_IMG_RGBVAR              (DIP_CODE(537))
+#define CODE_LED_UV                      (DIP_CODE(1001))
+#define CODE_LED_IR                      (DIP_CODE(1023))
+#define CODE_LED_BRAILLE                 (DIP_CODE(333))
+#define CODE_LED_POV                     (DIP_CODE(42))
+
 
 uint16_t read_state(spi_device_handle_t);
 void load_registers(spi_transaction_t *t);
