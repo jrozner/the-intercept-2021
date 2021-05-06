c74c01af689de08aecbaa7d5d7291e4e3a08d9a8
jk <jkthecjer@github.com>
2021-05-05 20:00:10 -0400

    shuffled dip settings

--- firmware/main/dip.h
+++ firmware/main/dip.h
@@ -20,11 +20,12 @@
 #define CODE_BUZZER_MORSE                (DIP_CODE(420))  // 0110100100
 #define CODE_BUZZER_ADVANCED_MORSE       (DIP_CODE(360))  // 0101101000
 #define CODE_INFRASOUND                  (DIP_CODE(831))  // 1100111111
-#define CODE_NUMBERS                     (DIP_CODE(833))
+#define CODE_NUMBERS                     (DIP_CODE(44))   // 0000101100
+#define CODE_NUMBERS2                    (DIP_CODE(357))  // 0101100101
 
 #define CODE_LED_IMG_BINARY              (DIP_CODE(164))  // 0010100100
 #define CODE_LED_IMG_REDVAR              (DIP_CODE(864))  // 1101100000
-#define CODE_LED_IMG_RGB                 (DIP_CODE(537))  // 1000011001
+#define CODE_LED_IMG_RGB                 (DIP_CODE(540))  // 1000011100
 #define CODE_LED_IMG_RGBVAR              (DIP_CODE(735))  // 1011011111
 #define CODE_LED_UV                      (DIP_CODE(1001)) // 1111101001
 #define CODE_LED_IR                      (DIP_CODE(1023)) // 1111111111
